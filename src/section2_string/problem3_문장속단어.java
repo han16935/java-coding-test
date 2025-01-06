4929751e7fb8fa78a1869f06ae10e8070076e4bd
han16935 <han1693516@gmail.com>
2025-01-06 14:49:00 +0900

    문장 속 단어 solve

---
+++ src/section2_string/problem3_문장속단어.java
@@ -0,0 +1,24 @@
+package section2_string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class problem3_문장속단어 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String sentence = reader.readLine();
+        String[] arr = sentence.split(" ");
+        int answerIndex = -1;
+        int maxLength = 0;
+
+        for(int i=0;i<arr.length;i++) {
+            if (arr[i].length() > maxLength) {
+                maxLength = arr[i].length();
+                answerIndex = i;
+            }
+        }
+
+        System.out.println(arr[answerIndex]);
+    }
+}
