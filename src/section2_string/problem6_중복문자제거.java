2db7d47902536db3576dca1c26fd9603b8759357
han16935 <han1693516@gmail.com>
2025-01-06 15:44:08 +0900

    중복문자제거 solve (HashSet 통해 )

---
+++ src/section2_string/problem6_중복문자제거.java
@@ -0,0 +1,26 @@
+package section2_string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class problem6_중복문자제거 {
+
+    public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       String str = br.readLine();
+       StringBuilder answer = new StringBuilder();
+       Set<Character> set = new HashSet<>();
+
+       for(int i=0;i<str.length();i++){
+           if(!set.contains(str.charAt(i))) {
+               set.add(str.charAt(i));
+               answer.append(str.charAt(i));
+           }
+       }
+
+       System.out.println(answer);
+    }
+}
