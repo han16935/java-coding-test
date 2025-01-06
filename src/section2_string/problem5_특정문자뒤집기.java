6b6ecbec9823abeb44fcda0010a8a7c257f2dc8e
han16935 <han1693516@gmail.com>
2025-01-06 15:15:19 +0900

    특정문자뒤집기 solve (중요!)

---
+++ src/section2_string/problem5_특정문자뒤집기.java
@@ -0,0 +1,28 @@
+package section2_string;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class problem5_특정문자뒤집기 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] s = br.readLine().toCharArray();
+        int start = 0;
+        int end = s.length-1;
+        while(start < end) {
+            if (Character.isAlphabetic(s[start]) && Character.isAlphabetic(s[end])){
+                char tmp = s[start];
+                s[start] = s[end];
+                s[end] = tmp;
+                start++;
+                end--;
+                continue;
+            }
+
+            if (!Character.isAlphabetic(s[start])) start++;
+            if (!Character.isAlphabetic(s[end])) end--;
+        }
+        System.out.println(s);
+    }
+}
