db873da3bf6aa8ec2148f962670863fb3231f4bc
han16935 <han1693516@gmail.com>
2025-01-10 18:39:25 +0900

    problem8_등수구하기 solve (틀려서 인프런 봄)

---
+++ src/inflearn/section3_array/problem8_등수구하기.java
@@ -0,0 +1,32 @@
+package inflearn.section3_array;
+
+import java.io.*;
+import java.util.*;
+
+public class problem8_등수구하기 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int [] arr = new int[n];
+        int i = 0;
+        while(st.hasMoreTokens()) {
+            arr[i++] = Integer.parseInt(st.nextToken());
+        }
+        solution(n, arr);
+    }
+
+    private static void solution(int n, int[] arr) {
+        int [] answer = new int[n];
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                if (arr[j] > arr[i]) answer[i]++;
+            }
+        }
+        for(int element : answer) {
+            System.out.print(element+1+" ");
+        }
+    }
+
+}
