a115dbd796bc2bdd6551b103cde649572fb94875
han16935 <han1693516@gmail.com>
2025-01-10 18:03:46 +0900

    problem7_점수계산 solve

---
+++ src/inflearn/section3_array/problem7_점수계산.java
@@ -0,0 +1,36 @@
+package inflearn.section3_array;
+
+import java.io.*;
+import java.util.*;
+
+public class problem7_점수계산 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int [] arr = new int[n+1];
+        int i = 1;
+        arr[0] = 0;
+        while(st.hasMoreTokens()) {
+            arr[i++] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(solution(n, arr));
+    }
+
+    private static int solution(int n, int[] arr) {
+        int answer = 0;
+        int score = 0;
+        for (int i=1;i<=n;i++) {
+            if (arr[i] == 1) {
+                score++;
+                answer+=score;
+            }
+
+            if (arr[i] == 0) {
+                score = 0;
+            }
+        }
+        return answer;
+    }
+}
