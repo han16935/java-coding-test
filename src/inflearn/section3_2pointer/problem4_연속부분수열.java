81fe0d77e3aaa8a824a86fea8b38ac111298f0bd
han16935 <han1693516@gmail.com>
2025-01-27 16:02:18 +0900

    연속부분수열 solve (투 포인터 사용)

--- src/inflearn/section3_2pointer/problem4_연속부분수열.java
+++ src/inflearn/section3_2pointer/problem4_연속부분수열.java
@@ -5,7 +5,12 @@ import java.util.*;
 
 /**
  * 투 포인터, 슬라이딩 윈도우 > O(n^2)를 O(n)으로 하고 싶을 때
+ * lt, rt를 기준으로 sum이 m보다 작으면 rt++ 하고 arr[rt] 더함
+ * m보다 크면 sum -= arr[lt] 하고 lt++함
+ * m과 같으면 answer++하고 lt 올림
  *
+ * 이렇게 rt가 배열 끝까지 갈 때까지 하지만, 만약 이 상황에서 sum이 m보다 클 경우, lt 쪽 계속 빼면
+ * sum == m인 경우 잇을 수도 있기 때문에 확인해봐야
  */
 public class problem4_연속부분수열 {
     public static void main(String[] args) throws IOException{
@@ -24,16 +29,34 @@ public class problem4_연속부분수열 {
 
     private static int solution(int n, int m, int[] arr) {
         int answer = 0;
-        for(int i=0;i<n;i++) {
-            int tmp = arr[i];
-            for(int j=i+1;j<n;j++) {
-                tmp += arr[j];
-                if (tmp >= m) {
-                    if (tmp == m) answer++;
-                    break;
-                }
+        int lt = 0;
+        int rt = 0;
+        int sum = arr[0];
+        while(rt < arr.length-1) {
+            if (sum < m) {
+                rt++;
+                sum += arr[rt];
+            }
+
+            else if (sum > m) {
+                sum -= arr[lt];
+                lt++;
+            }
+
+            else {
+                answer++;
+                sum -= arr[lt];
+                lt++;
             }
         }
+
+        while (sum > m) {
+           lt++;
+           sum -= arr[lt];
+        }
+
+        if (sum == m) answer++;
+
         return answer;
     }
 }
