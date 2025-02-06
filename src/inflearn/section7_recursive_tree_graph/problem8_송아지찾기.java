30d854068ae89beabe0c27d94a1958b6a98ed3fb
han16935 <han1693516@gmail.com>
2025-02-06 21:11:16 +0900

    송아지찾기 solve

--- src/inflearn/section7_recursive_tree_graph/problem8_송아지찾기.java
+++ src/inflearn/section7_recursive_tree_graph/problem8_송아지찾기.java
@@ -11,20 +11,29 @@ public class problem8_송아지찾기 {
         System.out.println(solution(s, e));
     }
 
+    /**
+     * distance 범위 (distance < 1 || distance > 10000) 통해서 Runtime error 및 time limit 막기!
+     *
+     */
     private static int solution(int s, int e) {
         Deque<Node> dq = new ArrayDeque<>();
+        boolean[] isVisited = new boolean[10001];
         dq.offer(new Node(0, s));
 
-        while(dq.peek().distance != e) {
+        while (!dq.isEmpty()) {
             Node polledNode = dq.poll();
             int depth = polledNode.depth;
             int distance = polledNode.distance;
 
-            dq.offer(new Node(depth+1, distance+1));
-            dq.offer(new Node(depth+1, distance-1));
-            dq.offer(new Node(depth+1, distance+5));
+            if (distance < 1 || distance > 10000 || isVisited[distance]) continue;
+            if (distance == e) return depth;
+            isVisited[distance] = true;
+
+            dq.offer(new Node(depth + 1, distance + 1));
+            dq.offer(new Node(depth + 1, distance - 1));
+            dq.offer(new Node(depth + 1, distance + 5));
         }
-        return dq.peek().depth;
+        return -1;
     }
 
     static class Node {
