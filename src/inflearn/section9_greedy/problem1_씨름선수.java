c7a27ae3cc422cb23fba0d3e70cfe7de10c1c2ee
han16935 <han1693516@gmail.com>
2025-04-22 12:11:32 +0900

    씨름선수 solve

--- src/inflearn/section9_greedy/problem1_씨름선수.java
+++ src/inflearn/section9_greedy/problem1_씨름선수.java
@@ -3,13 +3,21 @@ package inflearn.section9_greedy;
 import java.util.*;
 import java.io.*;
 
+/**
+ * 특정 선수보다 키, 몸무게 전부 보다 큰 사람이 없는 사람의 수 구하기
+ * 기존 풀이는 O(N^2) 로 풀었지만, 신규 풀이에서는 O(N) 으로 해결 가능!
+ *     - 키 기준으로 내림차순 정렬
+ *     - 이 때, maxWeight 통해 순회하면서 업데이트
+ *         - 만약 업데이트되었을 경우, 이전 선수 (그 선수보다 키가 큰 선수) 보다 무거운 선수는 없으므로 선출!
+ *         - 만약 업데이트되지 않았을 경우, 이전 선수 (그 선수보다 키가 큰 선수) 중 그 선수보다 무거운 선수가 있다는 의미이므로, 선출되지 않음
+ */
 public class problem1_씨름선수 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int answer = n;
+        int answer = 1; // 제일 키가 큰 선수
         Person[] persons = new Person[n];
 
         StringTokenizer st;
@@ -22,15 +30,14 @@ public class problem1_씨름선수 {
             persons[i] = new Person(height, weight);
         }
 
-        Arrays.sort(persons);
+        Arrays.sort(persons, Collections.reverseOrder());
 
-        for(int i=0;i<n-1;i++) {
+        int maxWeight = persons[0].weight;
+        for(int i=1;i<n;i++) {
             Person chosenPerson = persons[i];
-            for(int j=i+1;j<n;j++) {
-                if (persons[j].weight > chosenPerson.weight) {
-                    answer--;
-                    break;
-                }
+            if (chosenPerson.weight > maxWeight) {
+                answer++;
+                maxWeight = chosenPerson.weight;
             }
         }
 
