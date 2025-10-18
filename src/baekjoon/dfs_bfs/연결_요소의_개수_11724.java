3fc76830ad2b4567dec12bf4ec5cb43c75d4cdb2
han16935 <han1693516@gmail.com>
2025-10-18 11:20:32 +0900

    연결 요소의 개수 SOLVE

---
+++ src/baekjoon/dfs_bfs/연결_요소의_개수_11724.java
@@ -0,0 +1,53 @@
+package baekjoon.dfs_bfs;
+
+import java.util.*;
+import java.io.*;
+
+public class 연결_요소의_개수_11724 {
+    static int n, m;
+    static long answer;
+    static List<List<Integer>> graph = new ArrayList<>();
+    static boolean [] isVisited;
+
+    public static void main (String [] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        for (int i=0;i<=n;i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        isVisited = new boolean[n+1];
+        for (int i=0;i<m;i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            graph.get(start).add(end);
+            graph.get(end).add(start);
+        }
+
+        for (int start=1;start<=n;start++) {
+            if (!isVisited[start]) {
+                solution(start);
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    static void solution (int start) {
+
+        isVisited[start] = true;
+        for (int nodeIndex=0;nodeIndex<graph.get(start).size();nodeIndex++){
+            int destination = graph.get(start).get(nodeIndex);
+            if (!isVisited[destination]) {
+                solution(destination);
+            }
+        }
+    }
+}
