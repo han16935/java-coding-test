955afc3784c610e3fbc6e64219be5708d7c22026
han16935 <han1693516@gmail.com>
2025-10-27 22:15:37 +0900

    미로탐색 색종이붙이기 웹 내 풀이 실패로 인한 IDE 사용

--- src/baekjoon/dfs_bfs/미로탐색_2178_IMPORTANT.java
+++ src/baekjoon/dfs_bfs/미로탐색_2178_IMPORTANT.java
@@ -4,62 +4,61 @@ import java.util.*;
 import java.io.*;
 
 public class 미로탐색_2178_IMPORTANT {
+
     static int n, m;
     static int answer = Integer.MAX_VALUE;
-    static int [][] arr;
-    static boolean [][] isVisited;
+    static int [][] board;
     static int [] dn = {-1, 0, 1, 0}; // 12, 3, 6, 9
     static int [] dm = {0, 1, 0, -1}; // 12, 3, 6, 9
-    static Deque<Point> queue = new ArrayDeque<>();
+    static Deque<Node> queue = new ArrayDeque<>();
+    static boolean [][] isVisited;
 
     public static void main (String [] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        arr = new int[n + 1][m + 1];
-        isVisited = new boolean[n + 1][m + 1];
+        board = new int[n][m];
+        isVisited = new boolean[n][m];
 
-        for (int row = 1; row <=n; row ++) {
-            String line = br.readLine();
-            for (int col = 0; col < m; col ++) {
-                int num = line.charAt(col) - '0';
-                arr[row][col+1] = num;
+        for (int i=0;i<n;i++) {
+            String str = br.readLine();
+            for (int j=0;j<m;j++) {
+                board[i][j] = str.charAt(j) - '0';
             }
         }
 
-        isVisited[1][1] = true;
-        queue.offer(new Point(1, 1, 1));
+        isVisited[0][0] = true;
+        queue.offer(new Node(0, 0, 1));
         while (!queue.isEmpty()) {
-            Point polledPoint = queue.poll();
-            if (polledPoint.n == n && polledPoint.m == m) {
-                answer = Math.min(answer, polledPoint.count);
+
+            Node polledNode = queue.poll();
+            if (polledNode.n == n-1 && polledNode.m == m-1) {
+                answer = Math.min(answer, polledNode.count);
             }
 
-            else {
+            for (int i=0;i<4;i++) {
+                int nextN = polledNode.n + dn[i];
+                int nextM = polledNode.m + dm[i];
 
-                for (int i=0;i<4;i++) {
-                    int nextN = polledPoint.n + dn[i];
-                    int nextM = polledPoint.m + dm[i];
-                    if (nextN < 0 || nextN > n || nextM < 0 || nextM > m) continue;
-                    if (arr[nextN][nextM] == 0) continue;
-
-                    if (!isVisited[nextN][nextM]) {
-                        isVisited[nextN][nextM] = true; // offer 전에 check 함으로써 메모리 초과 방지
-                        queue.offer(new Point(nextN, nextM, polledPoint.count + 1));
-                    }
+                if (nextN < 0 || nextN >= n || nextM < 0 || nextM >= m) continue;
+                if (!isVisited[nextN][nextM] && board[nextN][nextM] == 1) {
+                    isVisited[nextN][nextM] = true;
+                    queue.offer(new Node(nextN, nextM, polledNode.count + 1));
                 }
             }
         }
+
         System.out.println(answer);
     }
 
-    static class Point {
+    static class Node {
         int n;
         int m;
         int count;
 
-        public Point (int n, int m, int count) {
+        public Node (int n, int m, int count) {
             this.n = n;
             this.m = m;
             this.count = count;
--- src/baekjoon/dfs_bfs/색종이붙이기_17136_IMPORTANT.java
+++ src/baekjoon/dfs_bfs/색종이붙이기_17136_IMPORTANT.java
@@ -6,71 +6,73 @@ import java.io.*;
 public class 색종이붙이기_17136_IMPORTANT {
 
     static int answer = Integer.MAX_VALUE;
-
     static int [][] board = new int[10][10];
-    static int [] paperCountArr = {0, 5, 5, 5, 5, 5}; // 0, 1*1, 2*2, ..., 5*5
+    static int [] paperCount = {0, 5, 5, 5, 5, 5};
 
     public static void main (String [] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st;
-        for (int i=0;i<10;i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j=0;j<10;j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
+        for (int row=0;row<10;row++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int col=0;col<10;col++) {
+                board[row][col] = Integer.parseInt(st.nextToken());
             }
         }
 
         solution(0, 0);
 
-        if (answer == Integer.MAX_VALUE) System.out.println(-1);
+        if (answer == Integer.MAX_VALUE) {
+            System.out.println(-1);
+        }
+
         else System.out.println(answer);
     }
 
-    static void solution(int xy, int paperCount) {
-
+    static void solution(int xy, int count) {
         if (xy == 100) {
-            answer = Math.min(answer, paperCount);
-            return;
+            answer = Math.min(answer, count);
         }
 
-        if (paperCount > answer) {
-            return;
-        }
+        else {
+            int x = xy / 10;
+            int y = xy % 10;
 
-        int x = xy / 10;
-        int y = xy % 10;
-        if (board[x][y] == 1) {
-            for (int paperType = 5; paperType > 0; paperType--) {
-                if (paperCountArr[paperType] > 0 && canFill(x, y, paperType)) {
-                    paperCountArr[paperType]--;
-                    fill(x, y, 0, paperType);
-                    solution(xy + 1, paperCount + 1);
-                    fill(x, y, 1, paperType);
-                    paperCountArr[paperType]++;
+            if (count > answer) return;
+            if (board[x][y] == 1) {
+                for (int paperIndex=5;paperIndex>=1;paperIndex--) {
+                    int paperSize = paperIndex;
+                    if (canFill(x, y, paperSize)) {
+                        fill(x, y, paperSize, 0);
+                        paperCount[paperIndex]--;
+                        solution(xy + 1, count + 1);
+                        fill(x, y, paperSize, 1);
+                        paperCount[paperIndex]++;
+                    }
                 }
             }
-        }
 
-        else solution (xy + 1, paperCount);
+            else solution(xy + 1, count);
+        }
     }
 
-    static boolean canFill(int x1, int y1, int paperType) {
-        if (x1 + paperType > 10 || y1 + paperType > 10) return false;
+    static boolean canFill(int x, int y, int paperSize) {
 
-        for (int x = x1; x < x1 + paperType; x++) {
-            for (int y = y1; y < y1 + paperType; y++) {
-                if (board[x][y] == 0) return false;
+        if (paperCount[paperSize] <= 0) return false;
+        if (x + paperSize > 10 || y + paperSize > 10) return false;
+
+        for (int row=x; row < x + paperSize;row++) {
+            for (int col=y; col < y + paperSize;col++) {
+                if(board[row][col] == 0) return false;
             }
         }
 
         return true;
     }
 
-    static void fill(int x1, int y1, int fillType, int paperType) {
-        for (int x = x1; x < x1 + paperType; x++) {
-            for (int y = y1; y < y1 + paperType; y++) {
-                board[x][y] = fillType;
+    static void fill(int x, int y, int paperSize, int fillType) {
+        for (int row=x; row < x + paperSize;row++) {
+            for (int col=y; col < y + paperSize;col++) {
+                board[row][col] = fillType;
             }
         }
     }
